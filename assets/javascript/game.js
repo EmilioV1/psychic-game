7131a92b407b6598732dfa5ff185216f6689e189
Emilio Valladares <vallae.wumbo@gmail.com>
2019-08-10 19:01:02 -0700

    Fixed bug with console not logging user key touches at all. Lower case function was not connected to the userGuess variable.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,15 @@
+var options = ["r", "p", "s"]
+
+// Listens for a key to be pressed.
 document.onkeyup = function(){
     var userGuess = string.fromCharCode(event.keycode)
         toLowerCase();
 
-    console.log(userGuess);
+  alert(userGuess);
+
+    var computerGuess = options[Math.floor(Math.random()*options.length)];
+
+    console.log(computerGuess);
+
+
 }
