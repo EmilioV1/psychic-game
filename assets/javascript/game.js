a18acfd516cc5fc5468057ec10bf4677cca70fb2
Emilio Valladares <vallae.wumbo@gmail.com>
2019-08-10 23:27:36 -0700

    Initial scoreboard added and counter logic added. Current bugs include user only being able to lose once and every keystroke after is a win. Still need to add win and lose restrictions with reset.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,37 @@
-var options = ["r", "p", "s"]
+
+// Variables Established
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var letterGuesses = [];
+
+// Loads scored board.
+document.getElementById("wins").innerHTML = "Wins:  " + wins;
+document.getElementById("losses").innerHTML = "Losses:  " + losses;
+document.getElementById("turns").innerHTML = "Turns Left:  " + numGuesses;
 
 // Listens for a key to be pressed.
 document.onkeyup = function(){
-    var userGuess = string.fromCharCode(event.keycode)
-        toLowerCase();
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  alert(userGuess);
+        // Updates Letters Guessed by user on page.
+        letterGuesses.push(userGuess);
+        document.getElementById("guesses").innerHTML = "Letters Guessed:  " + letterGuesses;
 
-    var computerGuess = options[Math.floor(Math.random()*options.length)];
+    var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
 
     console.log(computerGuess);
 
+    // Compares user and computer guess and adds to wins or losses.
+    if (userGuess === computerGuess, wins++){
 
+        document.getElementById("wins").innerHTML = "Wins:  " + wins;
+    } 
+        // If user and computer do not match increment losses and decrement numGuesses.
+        else if (userGuess !== computerGuess, losses++, numGuesses--){
+
+            document.getElementById("losses").innerHTML = "Losses:  " + losses;
+            document.getElementById("turns").innerHTML = "Turns Left:  " + numGuesses;
+        }
 }
