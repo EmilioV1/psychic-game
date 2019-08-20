34a495cb2a07db80110b35f1d3ce3334483d445a
Emilio Valladares <vallae.wumbo@gmail.com>
2019-08-19 20:37:57 -0700

    Bugs fixed with win/loss alerts not showing and bug with both wins and losses incrementing on every guess.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,10 @@ var losses = 0;
 var numGuesses = 9;
 var letterGuesses = [];
 
+// CHALLENGE!
+// Declare the random letter globally so it runs once on load.
+// Create a reset function that resets your global variables to whatever you declared them as, then call that in your win/loss conditions.
+
 // Loads scored board.
 document.getElementById("wins").innerHTML = "Wins:  " + wins;
 document.getElementById("losses").innerHTML = "Losses:  " + losses;
@@ -15,31 +19,33 @@ document.getElementById("guesses").innerHTML = "Letters Guessed:  " + letterGues
 // Listens for a key to be pressed.
 document.onkeyup = function(){
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
+    
         // Updates Letters Guessed by user on page.
         letterGuesses.push(userGuess);
         document.getElementById("guesses").innerHTML = "Letters Guessed:  " + letterGuesses;
 
     var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-
+    
     console.log(computerGuess);
 
     // Compares user and computer guess and adds to wins or losses.
-    if (userGuess = computerGuess, wins++){
-
+    if (userGuess === computerGuess){
+        wins++;
         document.getElementById("wins").innerHTML = "Wins:  " + wins;
     } 
         // If user and computer do not match increment losses and decrement numGuesses.
-        else if(userGuess !== computerGuess, losses++, numGuesses--){
-
+        else (userGuess !== computerGuess)
+            losses++; 
+            numGuesses--;
             document.getElementById("losses").innerHTML = "Losses:  " + losses;
             document.getElementById("turns").innerHTML = "Turns Left:  " + numGuesses;
-        }
+        
     // Check scoreboard for restrictions and reset game. 
     if(wins === 10){
         alert("Victory!")
-    }
-    if(losses === 9||numGuesses === 0){
+    };
+    if(losses === 9 || numGuesses === 0){
         alert("Game Over!")
-    }
-}
+    };
+
+};
